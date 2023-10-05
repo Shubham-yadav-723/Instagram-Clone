c059cbcab18f6c0f64ea4f39159424db7d9ed366
Shubham Yadav <72902202+Shubham-yadav-723@users.noreply.github.com>
2023-10-05 22:37:08 +0530

    done fetchng user detials

--- lib/responsive/mobile_Screen_layout.dart
+++ lib/responsive/mobile_Screen_layout.dart
@@ -1,14 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:insta_clone/provider/user_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:insta_clone/model/user.dart' as model;
 
 
-class mobileScreenLayout extends StatelessWidget {
+class mobileScreenLayout extends StatefulWidget {
   const mobileScreenLayout({super.key});
 
   @override
+  State<mobileScreenLayout> createState() => _mobileScreenLayoutState();
+}
+
+class _mobileScreenLayoutState extends State<mobileScreenLayout> {
+ 
+
+
+  @override
   Widget build(BuildContext context) {
+    model.User user=Provider.of<UserProvider>(context).getUser;
     return Scaffold(
-      body: Center(child: Text("this is mobile"),
+      body: Center(
+        child: Text(user.email),
       ),
     );
-}
+  }
 }
