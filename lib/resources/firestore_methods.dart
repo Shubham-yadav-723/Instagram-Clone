26dca66cbb813b4484578b09774cf5661ecefe4e
Shubham Yadav <72902202+Shubham-yadav-723@users.noreply.github.com>
2023-10-12 21:07:33 +0530

    done searching screen

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -78,4 +78,14 @@ class FireStoreMethods {
       );
     }
   }
+
+  //delete the post
+  Future<void> deletePost(String postId)async{
+    try {
+      await _firestore.collection('posts').doc(postId).delete();
+    } catch (e) {
+      print(e.toString());
+    }
+
+  }
 }
--- lib/utils/global_variable.dart
+++ lib/utils/global_variable.dart
@@ -3,12 +3,13 @@
 import 'package:flutter/material.dart';
 import 'package:insta_clone/screens/add_post.dart';
 import 'package:insta_clone/screens/feed_screen.dart';
+import 'package:insta_clone/screens/search_screen.dart';
 
 const webScreenSize=600;
 
 const homeScreenItems=[
           FeedScreen(),
-          Text("search"),
+          SearchScreen(),
           AddPostScreen(),
           Text("not"),
           Text("prog"),
